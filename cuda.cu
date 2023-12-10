d003cad7e290e100a75f5b17d0fb586c0d24a7d7
Amirreza Esmaeili <amirreza.77.esmaeili@gmail.com>
2023-12-10 13:43:05 -0800

    update alot of stuff

--- cuda.cu
+++ cuda.cu
@@ -3,120 +3,97 @@
 #include "util.cuh"
 #include <cuda_device_runtime_api.h>
 
-__global__ void cuda_hello(){
-    printf("Hello World from GPU!\n");
-}
-
-__device__ int d_abs(int a) {
-  return a > 0 ? a : -a;
-}
-
-__device__ void shift_pixel(UCHAR * r, UCHAR * g, UCHAR * b, int width, int height) {
-  r[0] = r[6231];
-  g[0] = g[6231];
-  b[0] = b[6231];
-}
-
-__device__ void negative_pixel(UCHAR * r, UCHAR * g, UCHAR * b) {
-  *r = 255 - *r;
-  *g = 255 - *g;
-  *b = 255 - *b;
-}
+__device__ int d_abs(int a) { return a > 0 ? a : -a; }
 
 #define span 1
 #define divider 8
 
-__device__ void energy_pixel(UCHAR * r, UCHAR * g, UCHAR * b, UCHAR * o_r, UCHAR * o_g, UCHAR * o_b, int width, int height) {
+__device__ void energy_pixel(UCHAR *r, UCHAR *g, UCHAR *b, UCHAR *o_r,
+                             UCHAR *o_g, UCHAR *o_b, int width, int height) {
 
-
-  o_r[0] = (
-          d_abs(r[0] - r[span])
-          + d_abs(r[0] - r[-span])
-          + d_abs(r[0] - r[width * span])
-          + d_abs(r[0] - r[-width * span])
-          + d_abs(r[0] - r[width * span + span])
-          + d_abs(r[0] - r[width * span - span])
-          + d_abs(r[0] - r[-width * span + span])
-          + d_abs(r[0] - r[-width * span - span])
-          ) / divider;
-  o_g[0] = (
-          d_abs(g[0] - g[span])
-          + d_abs(g[0] - g[-span])
-          + d_abs(g[0] - g[width * span])
-          + d_abs(g[0] - g[-width * span])
-          + d_abs(g[0] - g[width * span + span])
-          + d_abs(g[0] - g[width * span - span])
-          + d_abs(g[0] - g[-width * span + span])
-          + d_abs(g[0] - g[-width * span - span])
-          ) / divider;
-  o_b[0] = (
-          d_abs(b[0] - b[span])
-          + d_abs(b[0] - b[-span])
-          + d_abs(b[0] - b[width * span])
-          + d_abs(b[0] - b[-width * span])
-          + d_abs(b[0] - b[width * span + span])
-          + d_abs(b[0] - b[width * span - span])
-          + d_abs(b[0] - b[-width * span + span])
-          + d_abs(b[0] - b[-width * span - span])
-          ) / divider;
+  o_r[0] = (d_abs(r[0] - r[span]) + d_abs(r[0] - r[-span]) +
+            d_abs(r[0] - r[width * span]) + d_abs(r[0] - r[-width * span]) +
+            d_abs(r[0] - r[width * span + span]) +
+            d_abs(r[0] - r[width * span - span]) +
+            d_abs(r[0] - r[-width * span + span]) +
+            d_abs(r[0] - r[-width * span - span])) /
+           divider;
+  o_g[0] = (d_abs(g[0] - g[span]) + d_abs(g[0] - g[-span]) +
+            d_abs(g[0] - g[width * span]) + d_abs(g[0] - g[-width * span]) +
+            d_abs(g[0] - g[width * span + span]) +
+            d_abs(g[0] - g[width * span - span]) +
+            d_abs(g[0] - g[-width * span + span]) +
+            d_abs(g[0] - g[-width * span - span])) /
+           divider;
+  o_b[0] = (d_abs(b[0] - b[span]) + d_abs(b[0] - b[-span]) +
+            d_abs(b[0] - b[width * span]) + d_abs(b[0] - b[-width * span]) +
+            d_abs(b[0] - b[width * span + span]) +
+            d_abs(b[0] - b[width * span - span]) +
+            d_abs(b[0] - b[-width * span + span]) +
+            d_abs(b[0] - b[-width * span - span])) /
+           divider;
   UCHAR gray = o_r[0] * 0.3 + o_g[0] * 0.59 + o_b[0] * 0.11;
-  o_r[0] = gray;
-  o_g[0] = gray;
-  o_b[0] = gray;
+  o_r[0] = gray > 255 ? 255 : gray;
+  o_g[0] = gray > 255 ? 255 : gray;
+  o_b[0] = gray > 255 ? 255 : gray;
 }
 
-__global__ void negative_kernel(UCHAR * r, UCHAR * g, UCHAR * b,
-                                UCHAR * o_r, UCHAR * o_g, UCHAR * o_b, int width, int height, int * sig) {
-  *sig = *sig + 1;
+__global__ void energy_kernel(UCHAR *r, UCHAR *g, UCHAR *b, UCHAR *o_r,
+                              UCHAR *o_g, UCHAR *o_b, int width, int height) {
   int x = threadIdx.x + blockIdx.x * blockDim.x;
   int y = threadIdx.y + blockIdx.y * blockDim.y;
-  // if (x >= width - 1 || y >= height - 1 || x <= 1 || y <= 1) return;
+  if (x >= width || y >= height) {
+    return;
+  }
   int index = x + y * width;
-  // if (index > width * height - 1) return;
-  // r[index] = 255 - r[index];
-  // g[index] = 255 - g[index];
-  // b[index] = 255 - b[index];
-  energy_pixel(&r[index], &g[index], &b[index], &o_r[index], &o_g[index], &o_b[index], width, height);
+  energy_pixel(&r[index], &g[index], &b[index], &o_r[index], &o_g[index],
+               &o_b[index], width, height);
 }
 
-extern "C"
-void test() {
-  printf("Cuda test start\n");
-  cuda_hello<<<1,1>>>();
-  gpuErrchk( cudaPeekAtLastError() );
-  gpuErrchk( cudaDeviceSynchronize() );
-  printf("Cuda test done\n");
-}
-
-extern "C"
-void negative(UCHAR * r, UCHAR * g, UCHAR * b, int width, int height) {
-  int * sig;
-  dim3 threads(32, 32, 1);
-  dim3 blocks((width / 32), (height / 32), 1);
+extern "C" void energy(UCHAR *out_r, UCHAR *out_g, UCHAR *out_b, int width,
+                       int height) {
+  dim3 threads(32, 32);
+  dim3 blocks(((width - 1) / 32) + 1, ((height - 1) / 32) + 1);
   unsigned int pixelCount = width * height;
 
-  UCHAR * o_r;
-  UCHAR * o_g;
-  UCHAR * o_b;
-  cudaMalloc((void**)&o_r, pixelCount * sizeof(UCHAR));
-  cudaMalloc((void**)&o_g, pixelCount * sizeof(UCHAR));
-  cudaMalloc((void**)&o_b, pixelCount * sizeof(UCHAR));
-  cudaMemcpy(o_r, r, pixelCount * sizeof(UCHAR), cudaMemcpyDeviceToDevice);
-  cudaMemcpy(o_g, g, pixelCount * sizeof(UCHAR), cudaMemcpyDeviceToDevice);
-  cudaMemcpy(o_b, b, pixelCount * sizeof(UCHAR), cudaMemcpyDeviceToDevice);
+  UCHAR *original_r;
+  UCHAR *original_g;
+  UCHAR *original_b;
+  cudaMalloc((void **)&original_r, pixelCount * sizeof(UCHAR));
+  cudaMalloc((void **)&original_g, pixelCount * sizeof(UCHAR));
+  cudaMalloc((void **)&original_b, pixelCount * sizeof(UCHAR));
 
-  cudaMalloc(&sig, sizeof(int));
-  printf("Negative kernal starting ...\n");
-  negative_kernel<<<blocks, threads>>>(r, g, b, o_r, o_g, o_b, width, height, sig);
-  gpuErrchk( cudaPeekAtLastError() );
-  gpuErrchk( cudaDeviceSynchronize() );
+  cudaMemcpy(original_r, out_r, pixelCount * sizeof(UCHAR),
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(original_g, out_g, pixelCount * sizeof(UCHAR),
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(original_b, out_b, pixelCount * sizeof(UCHAR),
+             cudaMemcpyHostToDevice);
 
+  UCHAR *energy_r;
+  UCHAR *energy_g;
+  UCHAR *energy_b;
+  cudaMalloc((void **)&energy_r, pixelCount * sizeof(UCHAR));
+  cudaMalloc((void **)&energy_g, pixelCount * sizeof(UCHAR));
+  cudaMalloc((void **)&energy_b, pixelCount * sizeof(UCHAR));
 
-  cudaMemcpy(r, o_r, pixelCount * sizeof(UCHAR), cudaMemcpyDeviceToDevice);
-  cudaMemcpy(g, o_g, pixelCount * sizeof(UCHAR), cudaMemcpyDeviceToDevice);
-  cudaMemcpy(b, o_b, pixelCount * sizeof(UCHAR), cudaMemcpyDeviceToDevice);
+  energy_kernel<<<blocks, threads>>>(original_r, original_g, original_b,
+                                     energy_r, energy_g, energy_b, width,
+                                     height);
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
 
-  int h_sig = 8;
-  cudaMemcpy(&h_sig, sig, sizeof(int), cudaMemcpyDeviceToHost);
-  printf("Negative kernal finished. %d\n", h_sig);
+  cudaMemcpy(out_r, energy_r, pixelCount * sizeof(UCHAR),
+             cudaMemcpyDeviceToHost);
+  cudaMemcpy(out_g, energy_g, pixelCount * sizeof(UCHAR),
+             cudaMemcpyDeviceToHost);
+  cudaMemcpy(out_b, energy_b, pixelCount * sizeof(UCHAR),
+             cudaMemcpyDeviceToHost);
+
+  cudaFree(original_r);
+  cudaFree(original_g);
+  cudaFree(original_b);
+  cudaFree(energy_r);
+  cudaFree(energy_g);
+  cudaFree(energy_b);
 }
--- main.c
+++ main.c
@@ -1,101 +1,248 @@
 #include "cuda.cuh"
 #include "qdbmp.h"
 #include <cuda_device_runtime_api.h>
-#include <stdlib.h>
 #include <cuda_runtime.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <unistd.h>
+
+typedef struct {
+  int x;
+  int y;
+} point;
+
+int dfs_vertical(int x, int y, UCHAR *gray_channel, int bound_low,
+                 int bound_high, int width, int height, int total_energy,
+                 point *path) {
+  int index = x + y * width;
+  // gray_channel[index] = 0;
+  total_energy += gray_channel[index];
+  if (y == 0) {
+    path[y].x = x;
+    path[y].y = y;
+  }
+  if (y == height - 1) {
+    return total_energy;
+  }
+  int children_energy[] = {0, 0, 0};
+  int least_energy = 99999999;
+  int least_child_x = 0;
+  int i = 0;
+  for (int x_child = x - 1; x_child <= x + 1; ++x_child) {
+    if (x_child < bound_low || x_child >= bound_high) {
+      continue;
+    }
+    int next_energy =
+        dfs_vertical(x_child, y + 1, gray_channel, bound_low, bound_high, width,
+                     height, total_energy, path);
+    children_energy[i] = next_energy;
+    i++;
+  }
+  for (int i = 0; i < 3; i++) {
+    if (children_energy[i] < least_energy) {
+      least_energy = children_energy[i];
+      least_child_x = x + i - 1;
+    }
+  }
+  path[y].x = least_child_x;
+  path[y].y = y + 1;
+  return least_energy;
+}
+
+void find_best_path1(int width, int height, UCHAR *h_r,
+                     point *least_energy_paths) {
+  int least_total_energy = 99999999;
+  for (int batch = 0; batch < 1; batch++) {
+    int x = rand() % width;
+    point *path = malloc(height * sizeof(point));
+    int total_energy =
+        dfs_vertical(x, 0, h_r, x - 1, x + 1, width, height, 0, path);
+    if (total_energy < least_total_energy) {
+      least_total_energy = total_energy;
+      memcpy(least_energy_paths, path, height * sizeof(point));
+    }
+  }
+}
+void find_best_path2(int width, int height, int batch_size, UCHAR *h_r,
+                     point *least_energy_paths) {
+  int least_total_energy = 99999999;
+  for (int batch = 0; batch < batch_size; batch++) {
+    // int x = (width / 100) * batch;
+    int x = rand() % width;
+    int total_energy = 0;
+    point *path = malloc(height * sizeof(point));
+
+    for (int y = 0; y < height; ++y) {
+      int index = x + y * width;
+      UCHAR energy = h_r[index];
+      total_energy += energy * energy;
+      path[y].x = x;
+      path[y].y = y;
+      int min_child_energy = 255;
+      int rng = rand() % 31231412;
+
+      for (int j = 0; j < 3; ++j) {
+        int x_child = x + (rng + j) % 3 - 1;
+        if (x_child < 0 || x_child >= width) {
+          continue;
+        }
+        int index_child = x_child + (y + 1) * width;
+        UCHAR energy_child = h_r[index_child];
+        if (energy_child < min_child_energy) {
+          min_child_energy = energy_child;
+          x = x_child;
+        }
+      }
+    }
+    if (total_energy < least_total_energy) {
+      least_total_energy = total_energy;
+      memcpy(least_energy_paths, path, height * sizeof(point));
+    }
+  }
+}
 
 int main() {
   printf("Program start.\n");
-  test();
+  srand(time(NULL));
+
   BMP_GetError();
-  const char* inFile = "okanagan.bmp";
-	const char* outFile = "okanagan_processed2.bmp";
+  const char *inFileBase = "main";
+  char inFile[100];
+  sprintf(inFile, "%s.bmp", inFileBase);
+  char outColFile[100];
+  char outEnergyFile[100];
+  char outCroppedFile[100];
+  sprintf(outColFile, "%s_out_col.bmp", inFileBase);
+  sprintf(outEnergyFile, "%s_out_energy.bmp", inFileBase);
+  sprintf(outCroppedFile, "%s_out_cropped.bmp", inFileBase);
 
-	UINT width, height;
-	UINT x, y;
-	BMP* bmp;
+  int crop_percent = 25;
+  int batch_size = 10;
 
-	/* Read an image file */
-	bmp = BMP_ReadFile(inFile);
-	BMP_CHECK_ERROR(stdout, -1);
+  UINT width, height;
+  UINT x, y;
+  BMP *bmp;
+
+  bmp = BMP_ReadFile(inFile);
+  BMP_CHECK_ERROR(stdout, -1);
 
   printf("Image loaded.\n");
 
-	/* Get image's dimensions */
-	width = BMP_GetWidth(bmp);
-	height = BMP_GetHeight(bmp);
+  width = BMP_GetWidth(bmp);
+  height = BMP_GetHeight(bmp);
   unsigned int pixelCount = width * height;
-  BMP* bmpOut = BMP_Create(width, height, 32);
-  BMP_CHECK_ERROR( stderr, -2 );
+  BMP_CHECK_ERROR(stderr, -2);
 
   printf("Width: %d, Height: %d\n", (int)width, (int)height);
 
-  UCHAR * h_r = malloc(pixelCount * sizeof(UCHAR));
-  UCHAR * h_g = malloc(pixelCount * sizeof(UCHAR));
-  UCHAR * h_b = malloc(pixelCount * sizeof(UCHAR));
+  UCHAR *original_r = malloc(pixelCount * sizeof(UCHAR));
+  UCHAR *original_g = malloc(pixelCount * sizeof(UCHAR));
+  UCHAR *original_b = malloc(pixelCount * sizeof(UCHAR));
 
-  UCHAR * d_r;
-  UCHAR * d_g;
-  UCHAR * d_b;
-  cudaMalloc((void**)&d_r, pixelCount * sizeof(UCHAR));
-  cudaMalloc((void**)&d_g, pixelCount * sizeof(UCHAR));
-  cudaMalloc((void**)&d_b, pixelCount * sizeof(UCHAR));
-  
+  UCHAR *energy_r = malloc(pixelCount * sizeof(UCHAR));
+  UCHAR *energy_g = malloc(pixelCount * sizeof(UCHAR));
+  UCHAR *energy_b = malloc(pixelCount * sizeof(UCHAR));
+
+  UCHAR *cropped_r = malloc(pixelCount * sizeof(UCHAR));
+  UCHAR *cropped_g = malloc(pixelCount * sizeof(UCHAR));
+  UCHAR *cropped_b = malloc(pixelCount * sizeof(UCHAR));
+
   printf("H/D memory allocation done.\n");
 
-	/* Iterate through all the image's pixels */
-	for (y = 0; y < height; ++y) {
-		for (x = 0; x < width; ++x) {
-			/* Get pixel's RGB values */
+  for (y = 0; y < height; ++y) {
+    for (x = 0; x < width; ++x) {
       int index = x + y * width;
-			BMP_GetPixelRGB(bmp, x, y, h_r+index, h_g+index, h_b+index);
-		}
-	}
+      BMP_GetPixelRGB(bmp, x, y, original_r + index, original_g + index,
+                      original_b + index);
+      BMP_GetPixelRGB(bmp, x, y, energy_r + index, energy_g + index,
+                      energy_b + index);
+      BMP_GetPixelRGB(bmp, x, y, cropped_r + index, cropped_g + index,
+                      cropped_b + index);
+    }
+  }
 
-  cudaMemcpy(d_r, h_r, pixelCount * sizeof(UCHAR), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_g, h_g, pixelCount * sizeof(UCHAR), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b, h_b, pixelCount * sizeof(UCHAR), cudaMemcpyHostToDevice);
   printf("Data sent to device.\n");
 
-	for (y = 0; y < height; ++y) {
-		for (x = 0; x < width; ++x) {
-			/* Get pixel's RGB values */
+  energy(energy_r, energy_g, energy_b, width, height);
+
+  int desaturation = 5;
+  for (y = 0; y < height; ++y) {
+    for (x = 0; x < width; ++x) {
       int index = x + y * width;
-      *(h_r+index) = *(h_r+index)/10;
-      *(h_g+index) = *(h_g+index)/10;
-      *(h_b+index) = *(h_b+index)/10;
-		}
-	}
+      *(energy_r + index) = *(energy_r + index) / desaturation;
+      *(energy_g + index) = *(energy_g + index) / desaturation;
+      *(energy_b + index) = *(energy_b + index) / desaturation;
+    }
+  }
 
-  negative(d_r, d_g, d_b, width, height);
-  cudaDeviceSynchronize();
-
-  cudaMemcpy(h_r, d_r, pixelCount * sizeof(UCHAR), cudaMemcpyDeviceToHost);
-  cudaMemcpy(h_g, d_g, pixelCount * sizeof(UCHAR), cudaMemcpyDeviceToHost);
-  cudaMemcpy(h_b, d_b, pixelCount * sizeof(UCHAR), cudaMemcpyDeviceToHost);
   printf("Data received from device.\n");
 
-	for (y = 0; y < height; ++y) {
-		for (x = 0; x < width; ++x) {
-			/* Get pixel's RGB values */
+  int n_cols = width * crop_percent / 100;
+  point **least_energy_paths = malloc(n_cols * sizeof(point *));
+  for (int i = 0; i < n_cols; i++) {
+    least_energy_paths[i] = malloc(height * sizeof(point));
+    find_best_path2(width, height, batch_size, energy_r, least_energy_paths[i]);
+    for (int y = 0; y < height; ++y) {
+      int x = least_energy_paths[i][y].x;
       int index = x + y * width;
-      BMP_SetPixelRGB(bmpOut, x, y, *(h_r+index), *(h_g+index), *(h_b+index));
-		}
-	}
+      energy_r[index] = 255;
+      energy_g[index] = 0;
+      energy_b[index] = 0;
+    }
+  }
+  for (int i = 0; i < n_cols; i++) {
+    for (int y = 0; y < height; ++y) {
+      int x = least_energy_paths[i][y].x;
+      int index = x + y * width;
+      original_r[index] = 255;
+      original_g[index] = 0;
+      original_b[index] = 0;
+    }
+    free(least_energy_paths[i]);
+  }
 
-	/* Save result */
-	BMP_WriteFile(bmpOut, outFile);
-	BMP_CHECK_ERROR(stdout, -3);
+  BMP *bmpEnergy = BMP_Create(width, height, 32);
+  BMP *bmpOut = BMP_Create(width, height, 32);
+  for (y = 0; y < height; ++y) {
+    for (x = 0; x < width; ++x) {
+      int index = x + y * width;
+      BMP_SetPixelRGB(bmpEnergy, x, y, *(energy_r + index), *(energy_g + index),
+                      *(energy_b + index));
+      BMP_SetPixelRGB(bmpOut, x, y, *(original_r + index),
+                      *(original_g + index), *(original_b + index));
+    }
+  }
+  BMP *bmpCropped = BMP_Create(width - n_cols, height, 32);
+  for (y = 0; y < height; ++y) {
+    int reducer = 0;
+    for (x = 0; x < width; ++x) {
+      int index = x + y * width;
+      if (energy_r[index] == 255 && energy_g[index] == 0 &&
+          energy_b[index] == 0) {
+        reducer++;
+        continue;
+      }
+      BMP_SetPixelRGB(bmpCropped, x - reducer, y, *(original_r + index),
+                      *(original_g + index), *(original_b + index));
+    }
+  }
 
-	/* Free all memory allocated for the image */
-	BMP_Free(bmp);
-  cudaFree(d_r);
-  cudaFree(d_g);
-  cudaFree(d_b);
-  free(h_r);
-  free(h_g);
-  free(h_b);
+  BMP_WriteFile(bmpOut, outColFile);
+  BMP_CHECK_ERROR(stdout, -3);
+  BMP_WriteFile(bmpEnergy, outEnergyFile);
+  BMP_CHECK_ERROR(stdout, -3);
+  BMP_WriteFile(bmpCropped, outCroppedFile);
+  BMP_CHECK_ERROR(stdout, -3);
 
-	return 0;
+  BMP_Free(bmp);
+  cudaFree(energy_r);
+  cudaFree(energy_g);
+  cudaFree(energy_b);
+  free(energy_r);
+  free(energy_g);
+  free(energy_b);
+
+  return 0;
 }
